d809476065a9d5a921a0cc526605079d9ddb4a2a
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-18 20:39:40 -0400

    server update

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# 11 Express.js
+
+## Creating a Note Taker App
+
+This is the eleventh assignment for the MSU Coding Bootcamp; to modify starter code and create an application for writing and saving notes. It will use Express.js on the back end, and will use a JSON file to save and retrieve the data from the notes. 
+
+### Live Links
+- [Daniel Shoup's Note Taker Repisitory](https://github.com/danshoup/note-taker)
+- [Daniel Shoup's link the deployed file on Heroku](https://heroku)
+
+
+### Note Taker Screenshot
+
+![Application Screen Shot](./Assets/11-express-homework-demo-01.png)
+
+
+## Acceptance Criteria
+
+- When opened, a landing page is shown with a link to the notes page, which, in turn, takes the user to another page, with saved notes on one side, and a place to take new notes on the right side. 
+
+- When a new note is entered, a save button appears and the note is saved by clicking on the button; It then appears in the left-hand column with the other saved notes.
+
+- Clicking on a saved note in the left column moves it to the right column. An icon at the top of the page, when clicked, clears the input fields and sets them up for taking a new note.
+
+- Application front end must connect to an Express.js back end, and back end must store notes with unique IDs in a JSON file.
+
+- Application must be deployed to Heroku
+
+- Application must be deployed at a live URL, and load with no errors.
+
+- GitHub URL must be submitted, and the repository must contain the application code.
+
+- Application console is free of errors.
+
+- Repository has a unique name and follows best practices for naming, indentation, comments, and contains descriptive commit messages.  The repository also contains a quality READMY file with description, screenshot,a nd link to deployed application.
+
+- BONUS: Application allows users to delete notes.
+
+- URL of the functional, deployed application, and the URL of the GitHub repository must be submitted.
+
+
---
+++ server.js
@@ -0,0 +1,73 @@
+// Dependencies:
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let uniqid = require('uniqid');
+
+let notesField = [];
+
+// Sets up the express App:
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// Middleware:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// HTML Routes: 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// API Routes:
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+        err ? console.log(err): res.send(data);
+    });
+});
+
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            req.body.id = uniqid();
+            if (data) {
+                notesField = JSON.parse(data);
+            }
+            notesField.push(req.body);
+            fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesField), (err) => {
+                if (err) {console.log(err)};
+            });
+            res.end();
+        }
+    });
+});
+
+app.delete('api/notes:id', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let deleteId = req.params.id;
+            notesField = JSON.parse(data);
+            notesField = notesField.filter((note) => {
+                return note.id != deleteId;
+            });
+        };
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesField), (err) => {
+            if (err) {console.log(err)};
+        });
+        res.end();
+    });
+});
+
+// Starts server to begin listening:
+app.listen(PORT, () => console.log(`App is listening for NOTES on PORT ${PORT}`));
