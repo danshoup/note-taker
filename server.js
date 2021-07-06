e088fd4e602f6c6707e85531420cbc9f8d045c62
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-07-05 22:35:41 -0400

    fixed api post route and made code more dry

--- server.js
+++ server.js
@@ -2,58 +2,80 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-let uniqid = require('uniqid');
-// const noteData = require('./db/db.json');
-let notesArr = [];
+// Use uniqid to create a unique id for each note
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId();
+
+// Load Data
+let noteData = require('./db/db.json');
+
 
 // Sets up the express App:
 const app = express();
+
+// Sets up initial port for our listener.
 const PORT = process.env.PORT || 3030;
 
-// Middleware:
+// Middleware: set up to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
+// Sets express app to serve static assets directly
+app.use(express.static('public'));
 
 // API Routes:
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
-        err ? console.log(err): res.send(data);
-    });
-});
-
-
-app.post('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) {
             console.log(err);
         } else {
-            req.body.id = uniqid();
-            if (data) {
-                notesArr = JSON.parse(data);
-            }
-            notesArr.push(req.body);
-            fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesArr), 'utf8', (err) => {
-                if (err) {console.log(err)};
-            });
-            res.end();
-        }
+            res.send(data);
+        };
     });
 });
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = uid();
+    noteData.push(req.body);
+    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(noteData), (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(noteData);
+        };
+    });
+});
+
+// app.post('/api/notes', (req, res) => {
+//     fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             req.body.id = uid();
+//             noteData.push(req.body);
+//             fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(noteData), (err) => {
+//                 if (err) {
+//                     console.log(err)
+//                 } else {
+//                     res.send(noteData);
+//                 };
+//             });
+//         };
+//     });
+// });
+
 app.delete('api/notes/:id', (req, res) => {
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
         if (err) {
             console.log(err);
         } else {
             let deleteId = req.params.id;
-            notesArr = JSON.parse(data);
-            notesArr = notesArr.filter((note) => {
+            noteData = JSON.parse(data);
+            noteData = noteData.filter((note) => {
                 return note.id != deleteId;
             });
         };
-        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesArr), 'utf8', (err) => {
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(noteData), (err) => {
             if (err) {console.log(err)};
         });
         res.end();
@@ -61,15 +83,20 @@ app.delete('api/notes/:id', (req, res) => {
 });
 
 
-// HTML Routes: 
+// HTML GET Routes: handles when users "visit" a page.
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// // If no matching route is found default to home
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
 // Starts server to begin listening:
-app.listen(PORT, () => console.log(`App is listening for NOTES on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
